e14b35a63a43918313d8f7dbe7202ace5a0140be
Sarvesh Mandewal <63206965+SarveshNeel@users.noreply.github.com>
2021-07-26 14:17:13 +0530

    Update SegTree.cpp

--- SegTree.cpp
+++ SegTree.cpp
@@ -1,3 +1,5 @@
+
+//Range Sum Queries:
 struct SegTree{
     int base;
     vector<int> tree;
@@ -38,4 +40,42 @@ Note :
 2)tree positions are 1-based indexing
 3)query with 0....base-1 and not 0....n-1 in the root
 4)base is power of 2 >= n
+5) Do appropriate changes for range min queries
 */
+
+//Range Min Queries
+struct SegTree{
+    int base;
+    vector<int> tree;
+    void update(int i, int value){
+        int pos=base+i;
+        tree[pos]=value;
+        pos/=2;
+        while(pos>0){
+            tree[pos]=min(tree[2*pos],tree[2*pos+1]);
+            pos/=2;
+        }
+    }
+    int query(int ql, int qr, int nl, int nr, int node){
+        if(ql>nr || qr<nl){
+            return INF;
+        }
+        if(ql<=nl && nr<=qr){
+            return tree[node];
+        }
+        int mid=(nl+nr)/2;
+        return min(query(ql,qr,nl,mid,2*node),query(ql,qr,mid+1,nr,2*node+1));
+    }
+    SegTree(vector<int> a){
+        int n=a.size();
+        base=1;
+        while(2*base<n){
+            base*=2;
+        }
+        base*=2;
+        tree.resize(2*base,INF);
+        for(int i=0;i<n;i++){
+            update(i,a[i]);
+        }
+    }
+};
